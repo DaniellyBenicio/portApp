a65dc989820c13e41ea0a60dff7d525052f73fe6
joseolinda <joseolindad@yahoo.com.br>
2024-08-18 17:07:31 -0300

    fix imports and init firebase

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'Seletor.dart';
-import 'Register.dart';
+
+// Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -13,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Seletor(),
+      home: const Seletor(),
     );
   }
 }
