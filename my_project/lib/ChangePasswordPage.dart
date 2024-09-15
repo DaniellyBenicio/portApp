d9b0e5233adc8013cea1e34229c5607a7e5b1c93
DaniellyBenicio <daniellybeniciodearaujo@gmail.com>
2024-09-15 20:28:36 -0300

    changes of page changepassword

--- my_project/lib/ChangePasswordPage.dart
+++ my_project/lib/ChangePasswordPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'; 
 
 class ChangePasswordPage extends StatefulWidget {
   @override
