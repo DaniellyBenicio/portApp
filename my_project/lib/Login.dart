f400118088df0c856cb62e817c2c44babb7d28ea
Daniel Teixeira <115299209+DanielTeixeira23@users.noreply.github.com>
2024-09-19 22:10:18 -0300

    Update Login.dart

--- my_project/lib/Login.dart
+++ my_project/lib/Login.dart
@@ -81,171 +81,175 @@ class _LoginState extends State<Login> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context); // Ação do botão de voltar
-          },
-        ),
-        backgroundColor: Colors.blue,
-        elevation: 0,
-        
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context); // Ação do botão de voltar
+        },
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 35), 
-            const Text(
-              'Entre com E-mail',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      backgroundColor: Colors.blue,
+      elevation: 0,
+    ),
+    body: SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 35), 
+          const Text(
+            'Entre com E-mail',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'Faça login com o seu e-mail e senha para acessar sua conta.',
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 32),
+          Container(
+            width: 350, // Largura fixa dos campos
+            child: _buildTextField(
+              controller: _emailController,
+              label: 'Email',
+              isPassword: false,
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            width: 350, // Largura fixa dos campos
+            child: _buildTextField(
+              controller: _passwordController,
+              label: 'Senha',
+              isPassword: true,
+              obscureText: _obscurePassword,
+              onVisibilityChanged: (isVisible) {
+                setState(() {
+                  _obscurePassword = !isVisible;
+                });
+              },
+              onSubmitted: (value) {
+                _login(); // Chama a função de login ao pressionar Enter
+              },
+            ),
+          ),
+          SizedBox(height: 8), // Ajuste o espaço entre o campo de senha e o botão de esquecer senha
+          Align(
+            alignment: Alignment.center,
+            child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RecoverPassword()),
+                );
+              },
+              child: Text(
+                'Esqueceu sua senha?',
+                style: TextStyle(color: Colors.blue),
               ),
-              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 8),
-            const Text(
-              'Faça login com o seu e-mail e senha para acessar sua conta.',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 32),
-            Container(
-              width: 350, // Largura fixa dos campos
-              child: _buildTextField(
-                controller: _emailController,
-                label: 'Email',
-                isPassword: false,
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              width: 350, // Largura fixa dos campos
-              child: _buildTextField(
-                controller: _passwordController,
-                label: 'Senha',
-                isPassword: true,
-                obscureText: _obscurePassword,
-                onVisibilityChanged: (isVisible) {
-                  setState(() {
-                    _obscurePassword = !isVisible;
-                  });
-                },
-              ),
-            ),
-            SizedBox(height: 8), // Ajuste o espaço entre o campo de senha e o botão de esquecer senha
-            Align(
-              alignment: Alignment.center,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RecoverPassword()),
-                  );
-                },
-                child: Text(
-                  'Esqueceu sua senha?',
-                  style: TextStyle(color: Colors.blue),
+          ),
+          SizedBox(height: 16), // Ajuste o espaço entre o botão de esquecer senha e o botão de entrar
+          _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : SizedBox(
+                  width: 350, // Largura fixa do botão
+                  child: ElevatedButton(
+                    onPressed: _login,
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 16), // Ajuste o espaço entre o botão de esquecer senha e o botão de entrar
-            _isLoading
-                ? Center(child: CircularProgressIndicator())
-                : SizedBox(
-                    width: 350, // Largura fixa do botão
-                    child: ElevatedButton(
-                      onPressed: _login,
-                      child: Text(
-                        'Entrar',
-                        style: TextStyle(color: Colors.white),
+          SizedBox(height: 20), // Espaço antes do texto "Não possui uma conta?"
+          RichText(
+            text: TextSpan(
+              text: 'Não possui uma conta? ',
+              style: TextStyle(color: Colors.black),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Registre-se',
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: TapGestureRecognizer()..onTap = () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Register(userType: widget.userType),
                       ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                  ),
-            SizedBox(height: 20), // Espaço antes do texto "Não possui uma conta?"
-            RichText(
-              text: TextSpan(
-                text: 'Não possui uma conta? ',
-                style: TextStyle(color: Colors.black),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Registre-se',
-                    style: TextStyle(color: Colors.blue),
-                    recognizer: TapGestureRecognizer()..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Register(userType: widget.userType),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _buildTextField({
+  required TextEditingController controller,
+  required String label,
+  required bool isPassword,
+  bool obscureText = false,
+  ValueChanged<bool>? onVisibilityChanged,
+  ValueChanged<String>? onSubmitted, // Adicionando o parâmetro onSubmitted
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.black,
         ),
       ),
-    );
-  }
-
-  Widget _buildTextField({
-    required TextEditingController controller,
-    required String label,
-    required bool isPassword,
-    bool obscureText = false,
-    ValueChanged<bool>? onVisibilityChanged,
-  }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.black,
+      SizedBox(height: 8),
+      TextField(
+        controller: controller,
+        obscureText: isPassword ? obscureText : false,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
           ),
-        ),
-        SizedBox(height: 8),
-        TextField(
-          controller: controller,
-          obscureText: isPassword ? obscureText : false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: BorderSide(color: Colors.blue),
-            ),
-            suffixIcon: isPassword
-                ? IconButton(
-                    icon: Icon(
-                      obscureText ? Icons.visibility_off : Icons.visibility,
-                    ),
-                    onPressed: () {
-                      if (onVisibilityChanged != null) {
-                        onVisibilityChanged(obscureText);
-                      }
-                    },
-                  )
-                : null,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.blue),
           ),
+          suffixIcon: isPassword
+              ? IconButton(
+                  icon: Icon(
+                    obscureText ? Icons.visibility_off : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    if (onVisibilityChanged != null) {
+                      onVisibilityChanged(obscureText);
+                    }
+                  },
+                )
+              : null,
         ),
-      ],
-    );
-  }
+        onSubmitted: onSubmitted, // Chama o método quando Enter é pressionado
+      ),
+    ],
+  );
+}
 }
