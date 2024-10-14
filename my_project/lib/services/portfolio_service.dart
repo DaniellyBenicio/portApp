023d0636b3d4e6c10fe05b66ec2b54ef8415e697
LuanF11 <luaninfo09@gmail.com>
2024-10-14 14:00:06 -0300

    (portfolio_service):Criado metodos auxiliares

--- my_project/lib/services/portfolio_service.dart
+++ my_project/lib/services/portfolio_service.dart
@@ -2,31 +2,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PortfolioService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSnapshot? lastDocument, String userUid) async {
-  Query query = _firestore
-      .collection('Disciplinas')
-      .doc(disciplinaId)
-      .collection('Portfolios')
-      .orderBy('dataCriacao', descending: true);
 
-  if (lastDocument != null) {
-    query = query.startAfterDocument(lastDocument);
+  Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSnapshot? lastDocument, String userUid) async {
+    Query query = _firestore
+        .collection('Disciplinas')
+        .doc(disciplinaId)
+        .collection('Portfolios')
+        .orderBy('dataCriacao', descending: true);
+
+    if (lastDocument != null) {
+      query = query.startAfterDocument(lastDocument);
+    }
+
+    QuerySnapshot querySnapshot = await query.limit(10).get();
+
+    print('Documentos retornados: ${querySnapshot.docs.length}');
+
+    return _filterAndMapPortfolios(querySnapshot, userUid);
   }
-
-  QuerySnapshot querySnapshot = await query.limit(10).get();
-
-  // Verificar quais documentos foram retornados antes de filtrar
-  print('Documentos retornados: ${querySnapshot.docs.length}');
-
-  return querySnapshot.docs.where((doc) {
-    final data = doc.data() as Map<String, dynamic>;
-    // Adicionando uma verificação apenas para o professor
-    return data['professorUid'] == userUid; // Retornar apenas se for o professor
-  }).map((doc) => {
-    'id': doc.id,
-    ...doc.data() as Map<String, dynamic>
-  }).toList();
-}
 
   /// Método para adicionar um portfólio a uma disciplina
   Future<String> adicionarPortfolio({
@@ -35,9 +28,7 @@ Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSn
     required String descricao,
     required String professorUid,
   }) async {
-    if (titulo.isEmpty || descricao.isEmpty || professorUid.isEmpty) {
-      throw Exception('Título, descrição e UID do professor são obrigatórios.');
-    }
+    _validatePortfolioData(titulo, descricao, professorUid);
 
     try {
       final DocumentReference docRef = await _firestore.collection('Disciplinas')
@@ -87,14 +78,7 @@ Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSn
     required String alunoUid,
     required Map<String, dynamic> arquivoData,
   }) async {
-    final matriculas = await _firestore.collection('Matriculas')
-        .where('alunoUid', isEqualTo: alunoUid)
-        .where('disciplinaId', isEqualTo: disciplinaId) 
-        .get();
-
-    if (matriculas.docs.isEmpty) {
-      throw Exception('Aluno não matriculado na disciplina correspondente.');
-    }
+    await _validateStudentEnrollment(alunoUid, disciplinaId);
 
     try {
       await _firestore
@@ -128,7 +112,7 @@ Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSn
           .get();
 
       if (docSnapshot.exists) {
-        // Se o portfólio existir, realiza a exclusão
+      // Se o portfólio existir, realiza a exclusão
         await _firestore
             .collection('Disciplinas')
             .doc(disciplinaId)
@@ -164,36 +148,69 @@ Future<List<Map<String, dynamic>>> getPortfolios(String disciplinaId, DocumentSn
   }
 
   Future<List<Map<String, dynamic>>> getPortfoliosForStudent(String alunoUid) async {
-  // Primeiro, busque todas as matrículas do aluno
-  final matriculasSnapshot = await _firestore
-      .collection('Matriculas')
-      .where('alunoUid', isEqualTo: alunoUid)
-      .get();
-
-  // Obtenha todos os disciplinaIds
-  List<String> disciplinaIds = matriculasSnapshot.docs.map((doc) => doc['disciplinaId'] as String).toList();
-
-  List<Map<String, dynamic>> portfolios = [];
-
-  // Para cada disciplinaId, busque os portfólios
-  for (String disciplinaId in disciplinaIds) {
-    final portfoliosSnapshot = await _firestore
-        .collection('Disciplinas')
-        .doc(disciplinaId)
-        .collection('Portfolios')
-        .orderBy('dataCriacao', descending: true)
+    // Primeiro, busque todas as matrículas do aluno
+    final matriculasSnapshot = await _firestore
+        .collection('Matriculas')
+        .where('alunoUid', isEqualTo: alunoUid)
         .get();
 
-    // Adicione os portfólios encontrados à lista
+    // Obtenha todos os disciplinaIds
+    List<String> disciplinaIds = matriculasSnapshot.docs.map((doc) => doc['disciplinaId'] as String).toList();
+
+    List<Map<String, dynamic>> portfolios = [];
+
+    // Para cada disciplinaId, busque os portfólios
+    for (String disciplinaId in disciplinaIds) {
+      final portfoliosSnapshot = await _firestore
+          .collection('Disciplinas')
+          .doc(disciplinaId)
+          .collection('Portfolios')
+          .orderBy('dataCriacao', descending: true)
+          .get();
+      // Adicione os portfólios encontrados à lista
+      _addPortfolios(portfoliosSnapshot, portfolios);
+    }
+
+    return portfolios;
+  }
+
+  // Adiciona os portfólios do snapshot à lista de portfólios
+  void _addPortfolios(QuerySnapshot portfoliosSnapshot, List<Map<String, dynamic>> portfolios) {
     for (var doc in portfoliosSnapshot.docs) {
       portfolios.add({
         'id': doc.id,
-        ...doc.data()
+        ...doc.data() as Map<String, dynamic>
       });
     }
   }
 
-  return portfolios;
-}
+  // Filtra e mapeia os portfólios do snapshot para incluir apenas aqueles do professor especificado
+  List<Map<String, dynamic>> _filterAndMapPortfolios(QuerySnapshot querySnapshot, String userUid) {
+    return querySnapshot.docs.where((doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      return data['professorUid'] == userUid;
+    }).map((doc) => {
+      'id': doc.id,
+      ...doc.data() as Map<String, dynamic>
+    }).toList();
+  }
 
+  // Valida os dados do portfólio, garantindo que título, descrição e UID do professor não estejam vazios
+  void _validatePortfolioData(String titulo, String descricao, String professorUid) {
+    if (titulo.isEmpty || descricao.isEmpty || professorUid.isEmpty) {
+      throw Exception('Título, descrição e UID do professor são obrigatórios.');
+    }
+  }
+
+  // Valida se o aluno está matriculado na disciplina especificada
+  Future<void> _validateStudentEnrollment(String alunoUid, String disciplinaId) async {
+    final matriculas = await _firestore.collection('Matriculas')
+        .where('alunoUid', isEqualTo: alunoUid)
+        .where('disciplinaId', isEqualTo: disciplinaId)
+        .get();
+
+    if (matriculas.docs.isEmpty) {
+      throw Exception('Aluno não matriculado na disciplina correspondente.');
+    }
+  }
 }
